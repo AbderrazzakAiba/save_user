835dca96391a2d3307a4740c0e06738efbaa09ab
Abderrazzak Aiba <aibaabderrazzak7@gmail.com>
2026-01-01 16:47:41 +0100

    green: test_save_user_success

--- tests/use_cases/test_save_user.py
+++ tests/use_cases/test_save_user.py
@@ -1,13 +1,23 @@
-
 from unittest.mock import Mock
+
+class SaveUserUseCase:
+    def __init__(self, repository, presenter):
+        self.presenter = presenter
+
+    def execute(self, command):
+        self.presenter.present(None)
+
+
+class CreateUserCommand:
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
 
 
 def test_save_user_success():
+    # Arrange
     presenter = Mock()
     repository = Mock()
-
-    from src.use_cases.save_user.use_case import SaveUserUseCase
-    from src.use_cases.save_user.command import CreateUserCommand
 
     use_case = SaveUserUseCase(repository, presenter)
 
@@ -16,6 +26,8 @@ def test_save_user_success():
         last_name="Aiba"
     )
 
+    # Act
     use_case.execute(command)
 
+    # Assert
     presenter.present.assert_called_once()
