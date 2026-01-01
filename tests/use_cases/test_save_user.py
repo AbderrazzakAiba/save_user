b0a7c541cbd481fb86353bf05a5dc9a04bec7077
Abderrazzak Aiba <aibaabderrazzak7@gmail.com>
2026-01-01 17:35:12 +0100

    Red:test_save_user_when_user_already_exists

--- tests/use_cases/test_save_user.py
+++ tests/use_cases/test_save_user.py
@@ -2,12 +2,13 @@ from unittest.mock import Mock
 
 from src.use_cases.save_user.use_case import SaveUserUseCase
 from src.use_cases.save_user.command import CreateUserCommand
+from src.use_cases.save_user.output_dto import OutputDTO
 
 
 def test_save_user_success():
-    # Arrange
     presenter = Mock()
     repository = Mock()
+    repository.exists.return_value = False
 
     use_case = SaveUserUseCase(repository, presenter)
 
@@ -16,8 +17,27 @@ def test_save_user_success():
         last_name="Aiba"
     )
 
-    # Act
     use_case.execute(command)
 
-    # Assert
-    presenter.present.assert_called_once()
+    presenter.present.assert_called_once_with(
+        OutputDTO(success=True)
+    )
+
+
+def test_save_user_when_user_already_exists():
+    presenter = Mock()
+    repository = Mock()
+    repository.exists.return_value = True
+
+    use_case = SaveUserUseCase(repository, presenter)
+
+    command = CreateUserCommand(
+        first_name="Abderrazzak",
+        last_name="Aiba"
+    )
+
+    use_case.execute(command)
+
+    presenter.present.assert_called_once_with(
+        OutputDTO(success=False)
+    )
