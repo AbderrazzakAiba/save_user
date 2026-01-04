21edd811456e3a256c49e111d8e572fe44693e07
Abderrazzak Aiba <aibaabderrazzak7@gmail.com>
2026-01-04 15:19:17 +0100

    Red: test_name_cannot_contain_digits

--- tests/domain/test_name.py
+++ tests/domain/test_name.py
@@ -6,3 +6,10 @@ def test_name_cannot_be_empty():
 
     with pytest.raises(InvalidNameError):
         Name("")
+
+
+def test_name_cannot_contain_digits():
+    from src.domain.name import Name, InvalidNameError
+
+    with pytest.raises(InvalidNameError):
+        Name("Ali1")
