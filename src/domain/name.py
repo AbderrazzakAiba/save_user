7cc4302bddd49988033c489a317ab249fa78448a
Abderrazzak Aiba <aibaabderrazzak7@gmail.com>
2026-01-04 14:45:18 +0100

    green: test_name_cannot_be_empty

---
+++ src/domain/name.py
@@ -0,0 +1,10 @@
+class InvalidNameError(Exception):
+    pass
+
+
+class Name:
+    def __init__(self, value: str):
+        if value == "":
+            raise InvalidNameError()
+
+        self.value = value
